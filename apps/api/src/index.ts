67ea3e1c1083cbca92fdb553a49b1bfa4b29e991
Devendra Sengar <devendrasengar8989@gmail.com>
2026-08-05 10:30:34 +0530

    Update index.ts

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; 
 import { connectDB } from './config/db';
 import authRoutes from './routes/auth.routes'; 
 import marketplaceRoutes from './routes/marketplace.routes';  
