eeae23f917d817215aa939a33b86c76062cb4c31
Devendra Sengar <devendrasengar8989@gmail.com>
2026-08-29 09:08:39 +0530

    Update fix_warden_permissions.js

--- apps/api/fix_warden_permissions.js
+++ apps/api/fix_warden_permissions.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/nexstay').then(async () => {
   const db = mongoose.connection.db;
   
-  // Update all WARDEN users to have canUploadMenu: true
+  // Update all WARDEN users to have canUploadMenu: true 
   const res = await db.collection('users').updateMany(
     { role: 'WARDEN' },
     { $set: { 'staffPermissions.canUploadMenu': true } }
